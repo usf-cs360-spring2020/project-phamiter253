34bad0830f018b98b8e6a3fed0f3972f5804eaf7
Priscilla Hamiter <phamiter253@gmail.com>
2020-04-14 16:05:31 -0700

    adding everything

---
+++ network.js
@@ -0,0 +1,130 @@
+    var width = 600,
+      height = 600,
+      start = 0,
+      end = 2.25,
+      numSpirals = 3
+      margin = {top:50,bottom:50,left:50,right:50};
+
+    var theta = function(r) {
+      return numSpirals * Math.PI * r;
+    };
+
+    // used to assign nodes color by group
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    var r = d3.min([width, height]) / 2 - 40;
+
+    var radius = d3.scaleLinear()
+      .domain([start, end])
+      .range([40, r]);
+
+    var svg = d3.select("#vis").append("svg")
+      .attr("width", width + margin.right + margin.left)
+      .attr("height", height + margin.left + margin.right)
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var points = d3.range(start, end + 0.001, (end - start) / 1000);
+
+    var spiral = d3.radialLine()
+      .curve(d3.curveCardinal)
+      .angle(theta)
+      .radius(radius);
+
+    var path = svg.append("path")
+      .datum(points)
+      .attr("id", "spiral")
+      .attr("d", spiral)
+      .style("fill", "none")
+      .style("stroke", "steelblue");
+
+
+  d3.json("csvjson.json").then(function(csv){
+    console.log(csv)
+    drawBars(csv)
+  });
+
+  d3.csv("profile.csv").then(function(csv){
+    var hierarchy = d3.nest()
+    .key(function(d) { return d.gender; })
+    .key(function(d) { return d.favorites; })
+    .entries(data);
+
+var stuff = (JSON.stringify(hierarchy));
+console.log(stuff);
+  });
+
+
+  function drawBars(json) {
+    var spiralLength = path.node().getTotalLength(),
+        N = 365,
+        barWidth = (spiralLength / N) - 1;
+    var someData = [];
+    for (var i = 0; i < N; i++) {
+      var item = json[i];
+      var score = item.score / 10;
+      console.log(item.title);
+      console.log(item.genre);
+      var currentDate = new Date();
+      currentDate.setDate(currentDate.getDate() + i);
+      someData.push({
+        date: currentDate,
+        value: score,
+        group: item.genre
+      });
+    }
+
+    var timeScale = d3.scaleLinear()
+      .domain(d3.extent(someData, function(d){
+        return d.date;
+      }))
+      .range([0, spiralLength]);
+    
+    // yScale for the bar height
+    var yScale = d3.scaleLinear()
+      .domain([0, d3.max(someData, function(d){
+        return d.value;
+      })])
+      .range([0, (r / numSpirals) - 30]);
+
+    svg.selectAll("rect")
+      .data(someData)
+      .enter()
+      .append("rect")
+      .attr("x", function(d,i){
+        
+        var linePer = timeScale(d.date),
+            posOnLine = path.node().getPointAtLength(linePer),
+            angleOnLine = path.node().getPointAtLength(linePer - barWidth);
+      
+        d.linePer = linePer;
+        //console.log("linePer", linePer); // % distance are on the spiral
+        d.x = posOnLine.x; // x postion on the spiral
+        d.y = posOnLine.y; // y position on the spiral
+        
+        d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
+
+        return d.x;
+      })
+      .attr("y", function(d){
+        return d.y;
+      })
+      .attr("width", function(d){
+        return barWidth;
+      })
+      .attr("height", function(d){
+        return yScale(d.value);
+      })
+      .style("fill", function(d){return color(d.group);})
+      .style("stroke", "none")
+      .attr("transform", function(d){
+        return "rotate(" + d.a + "," + d.x  + "," + d.y + ")"; // rotate the bar
+      });
+    
+    // add date labels
+    var tF = d3.timeFormat("%b %Y"),
+        firstInMonth = {};
+
+  }
+
+    
